bdda9e305485c848d44fbf974b7e84b5becb2fe6
ak1103dev <ak.apichan1103@gmail.com>
2017-05-11 19:42:52 +0700

    unit test isLegal

--- src/routes/ships.js
+++ src/routes/ships.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const Joi = require('joi');
 const _ = require('lodash');
+const isLegal = require('../utils/isLegal');
 const { Ship, Config } = require('../models/');
 
 const fleet = ['submarine', 'destroyer', 'cruiser', 'battleship'];
@@ -16,29 +17,6 @@ const placeShip = (board, positions) => {
   const newBoard = board;
   positions.map((position) => (board[position] = true));
   return newBoard;
-};
-
-const isLegal = (board, positions) => {
-  let checkingArea = [];
-  positions.map((p) => {
-    if (p === 0) return (checkingArea = _.union(checkingArea, [0, 1, 10, 11]));
-    if (p === 9) return (checkingArea = _.union(checkingArea, [8, 9, 18, 19]));
-    if (p === 90) return (checkingArea = _.union(checkingArea, [90, 91, 80, 81]));
-    if (p === 99) return (checkingArea = _.union(checkingArea, [98, 99, 88, 89]));
-    if (p / 10 === 0) return (checkingArea = _.union(checkingArea, [p - 1, p, p + 1, p + 9, p + 10, p + 11]));
-    if (p / 10 === 9) return (checkingArea = _.union(checkingArea, [p - 1, p, p + 1, p - 11, p - 10, p - 9]));
-    if (p % 10 === 0) return (checkingArea = _.union(checkingArea, [p - 10, p, p + 10, p - 9, p + 1, p + 11]));
-    if (p % 10 === 9) return (checkingArea = _.union(checkingArea, [p - 10, p, p + 10, p - 11, p - 1, p + 9]));
-    return (checkingArea = _.union(checkingArea, [
-      p - 11, p - 10, p - 9,
-      p - 1, p, p + 1,
-      p + 9, p + 10, p + 11
-    ]));
-  });
-  // console.log(checkingArea);
-  let legal = false;
-  checkingArea.map((p) => (legal = legal || board[p]));
-  return !legal;
 };
 
 const router = Router();
---
+++ test/isLegal.unit.test.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+const { expect } = require('chai');
+const isLegal = require('../src/utils/isLegal');
+
+const board = _.times(100, _.constant(false));
+
+describe('legal or illegal placement', () => {
+  it('it should be legal because of empty board', () => {
+    const expectedValue = true;
+    const input = [12, 13, 14];
+    expect(isLegal(board, input)).to.equal(expectedValue);
+  });
+  it('it should be illegal because ship overlap', () => {
+    const expectedValue = false;
+    const input = [12, 13, 14];
+    const newBoard = board;
+    board[12] = true;
+    expect(isLegal(newBoard, input)).to.equal(expectedValue);
+  });
+  it('it should be illegal because ship is diagonally adjacent', () => {
+    const expectedValue = false;
+    const input = [12, 13, 14];
+    const newBoard = board;
+    board[5] = true;
+    expect(isLegal(newBoard, input)).to.equal(expectedValue);
+  });
+  it('it should be illegal because ship is horizontally adjacent', () => {
+    const expectedValue = false;
+    const input = [12, 13, 14];
+    const newBoard = board;
+    board[11] = true;
+    expect(isLegal(newBoard, input)).to.equal(expectedValue);
+  });
+  it('it should be illegal because ship is vertically adjacent', () => {
+    const expectedValue = false;
+    const input = [12, 13, 14];
+    const newBoard = board;
+    board[23] = true;
+    expect(isLegal(newBoard, input)).to.equal(expectedValue);
+  });
+});
